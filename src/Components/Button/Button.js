e7aa0327ac79b2d7ab66a6267d4c8b67fd3f1045
Carlos Pajares <c.pajares@hotmail.com>
2021-10-20 21:54:40 +0200

    created Keyboard component

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import phoneContext from "../Context/Context";
 
-const Button = ({ text }) => {
+const Button = ({ text, className }) => {
   const { marcar } = useContext(phoneContext);
   return (
     <li>
-      <button onClick={marcar} className="key">
+      <button onClick={marcar} className={className}>
         {text}
       </button>
     </li>
