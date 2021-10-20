4797eab34a70973d251e7ddd81f35173c66cddef
Carlos Pajares <c.pajares@hotmail.com>
2021-10-20 21:08:42 +0200

    created useContext - phoneContext

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,4 +1,8 @@
-const Button = ({ text, marcar }) => {
+import { useContext } from "react";
+import phoneContext from "../Context/Context";
+
+const Button = ({ text }) => {
+  const { marcar } = useContext(phoneContext);
   return (
     <li>
       <button onClick={marcar} className="key">
